eb1d7e385361634fb9abd2443cdb1921b0769afd
Renan Martins Conceição Silva de Lima <92929174+Renanmali@users.noreply.github.com>
2023-07-01 04:22:24 -0300

    Comentando a main para criar a interface
    Updates

--- Main.java
+++ Main.java
@@ -12,17 +12,17 @@ public class Main {
     public static void main(String[] args) {
         // carregaUsuarios();
         // Criando o banco de músicas //
-        BancoMusicas bancoMusicas = new BancoMusicas();
-        BancoMusicas.addMusica(new MusicaCancao("Fim de Semana no Rio", "21/06/2023", "Teto", "rap", 300, "Olá"));
+        //BancoMusicas bancoMusicas = new BancoMusicas();
+        //BancoMusicas.addMusica(new MusicaCancao("Fim de Semana no Rio", "21/06/2023", "Teto", "rap", 300, "Olá"));
         //BancoMusicas.addMusica(new MusicaInstrumental("Rio de Janeiro", "23/06/2023", "Xamã", "rap", 400, "Sol"));
         // System.out.println(BancoMusicas.getById(0).getData());
         // // System.out.println(BancoMusicas.getById(1));
         // UsuarioComum user = new UsuarioComum("renan", "renanmali", "151021Al");
         // UsuarioAdmin userAdm = new UsuarioAdmin("Pedro", "Pierrot", "8238120");
         // BancoUsuarios bancoUsuarios = new BancoUsuarios("Spotify");
-        bancoMusicas.saveMusicas();
-        bancoMusicas.readMusicas();
-        System.out.println(BancoMusicas.getById(0).getTitulo());
+        //bancoMusicas.saveMusicas();
+        //bancoMusicas.readMusicas();
+        //System.out.println(BancoMusicas.getById(0).getTitulo());
         // System.out.println(BancoMusicas.getById(1).getData());
         // bancoUsuarios.addUsuario(user);
         // bancoUsuarios.addUsuario(userAdm);
